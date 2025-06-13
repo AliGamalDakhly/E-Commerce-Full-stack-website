e5862e4814b75bf2e1bbe3593b41e66c797368d5
Ali Gamal <aligamaldakhly99@gmail.com>
2025-06-13 16:11:40 +0300

    Added Category Service , That Communicate with Repository to get and manipulate required data.

---
+++ Bussiness_Logic_Layer/IServices/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Data_Access_Layer.Entities;
+
+namespace Bussiness_Logic_Layer.IServices
+{
+    public interface ICategoryService
+    {
+        IEnumerable<Category> GetAll(Expression<Func<Category, bool>>? filter = null, params Expression<Func<Category, object>>[] includes);
+        Category AddCategory(Category category);
+        Category UpdateCategory(Category category);
+        bool DeleteCategory(int? id);
+        Category GetCategoryById(int? id);
+    }
+}
