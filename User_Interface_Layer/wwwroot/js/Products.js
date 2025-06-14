dd190f99fff5ad820749ac106526aeed40b701f9
Ali Gamal <aligamaldakhly99@gmail.com>
2025-06-14 21:44:51 +0300

    added bootstreap libraries.

---
+++ User_Interface_Layer/wwwroot/js/Products.js
@@ -0,0 +1,20 @@
+﻿var dTable;
+
+$(document).ready(function () {
+    LoadData();
+});
+
+function LoadData() {
+    dTable = $("#ProductsTable").DataTable({
+        "ajax": {
+            "url": "/Admin/Product/GetDataTable"
+        },
+        "columns": [
+            { "data": "name" },
+            { "data": "description" },
+            { "data": "price" },
+            { "data": "category.name" },
+            { "data": "createddate" }
+        ]
+    })
+}
